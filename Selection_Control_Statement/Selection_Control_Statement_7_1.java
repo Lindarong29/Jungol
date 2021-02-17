6b8075a48e05a9529f937cd95e57014fbf3731e1
Lindarong <76677316+Lindarong29@users.noreply.github.com>
2021-02-17 23:49:40 +0900

    Update Selection_Control_Statement_7_1.java
    // if 문 사용 시, 왜 모든 입력에 error 출력 나오는 지 모르겠음..

--- Selection_Control_Statement/Selection_Control_Statement_7_1.java
+++ Selection_Control_Statement/Selection_Control_Statement_7_1.java
@@ -1,13 +1,15 @@
+// 왜 모든 입력에 error 출력 나오는 지 모르겠음.. 
+
 import java.util.*;
 
 
-/*���� �빮�ڸ� �Է¹޾� 
-'A'�̸� ��Excellent��, 
-'B'�̸� ��Good��,
-'C'�̸� ��Usually��, 
-'D'�̸� ��Effort��, 
-'F'�̸� ��Failure��, 
-�� �� �����̸� ��error�� ��� ����ϴ� ���α׷��� �ۼ��Ͻÿ�.*/
+/*영문 대문자를 입력받아 
+'A'이면 “Excellent”, 
+'B'이면 “Good”,
+'C'이면 “Usually”, 
+'D'이면 “Effort”, 
+'F'이면 “Failure”, 
+그 외 문자이면 “error” 라고 출력하는 프로그램을 작성하시오.*/
 
 public class Selection_Control_Statement_7 {
 
